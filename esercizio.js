8860ae003a9ecae3d58572d2dbec12abd6c6fd09
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2023-11-23 18:47:11 +0100

    first commit

---
+++ esercizio.js
@@ -0,0 +1,5 @@
+let operazione=1
+
+while (operazione>0){
+    operazione = parseInt(prompt("1:Aggiungi elemento\n2:Elimina un elemento dalla lista\n3:Ordina la lista in ordine alfabetico\n4:Modifica un elemento della lista\n5:Elimina l’intera lista\n6:Visualizza la lista"))
+}
